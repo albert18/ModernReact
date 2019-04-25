401f85b4952adae72adaeb1c4327f945b67d67c5
albert18 <caps18@hotmail.com>
2019-04-25 00:25:57 -0400

    2.19

--- Section2/jsx/src/index.js
+++ Section2/jsx/src/index.js
@@ -4,7 +4,12 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return <div>Hi There</div>;
+    return (
+    <div>
+        <label className="label" for="name">Enter Name:</label>
+        <input id="name" text="text"/>
+        <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+    </div>)
 };
 
 // Take the react component and show it on to screen
