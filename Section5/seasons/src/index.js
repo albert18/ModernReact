51172bd186be8701ec244a7b2ae7aecb826eef3e
albert18 <caps18@hotmail.com>
2019-05-06 19:22:31 -0400

    5.4

--- Section5/seasons/src/index.js
+++ Section5/seasons/src/index.js
@@ -1,14 +1,26 @@
+/* eslint-disable no-useless-constructor */
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
+    constructor(props) {
+        super(props);
 
-        return  <div>Latitude: </div>
+        // This is the only time we do the direct
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // We called setState!!
+                this.setState({ lat: position.coords.latitude });
+            },
+            err => console.log(err)
+        );
+    }
+
+    //React says we have to define render!!!
+    render() {
+        return  <div>Latitude: { this.state.lat }</div>
     }
 }
 
