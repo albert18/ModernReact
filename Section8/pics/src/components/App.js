8742490dfc17844e1a64e369f77970bb1fb4e108
albert18 <caps18@hotmail.com>
2019-05-12 22:43:57 -0400

    8.90

--- Section8/pics/src/components/App.js
+++ Section8/pics/src/components/App.js
@@ -6,7 +6,10 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
     onSearchSubmit(term) {
         axios.get('https://api.unsplash.com/search/photos', {
-            
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID 3309e3bf897959a95859cd536ca50cb581b98d4e99be045572a862a27a2eb3cb'
+            }
         });
     }
 
