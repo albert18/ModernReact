d1e6da731776c88c867245f26587e833ad628a2c
albert18 <caps18@hotmail.com>
2019-05-12 23:06:12 -0400

    8.92

--- Section8/pics/src/components/App.js
+++ Section8/pics/src/components/App.js
@@ -4,6 +4,8 @@ import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
+    state = { images: [] };
+
     async onSearchSubmit(term) {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
@@ -11,14 +13,15 @@ class App extends React.Component {
                 Authorization: 'Client-ID 3309e3bf897959a95859cd536ca50cb581b98d4e99be045572a862a27a2eb3cb'
             }
         });
-
-        console.log(response.data.results)
+        console.log(this);
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
             <SearchBar onSubmit={this.onSearchSubmit} />
+            Found: {this.state.images.length} images
         </div>
         );
     }
