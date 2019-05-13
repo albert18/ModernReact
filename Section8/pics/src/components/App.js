a4bdb1a5eef708440451f9da21508aec7af129a0
albert18 <caps18@hotmail.com>
2019-05-12 22:57:01 -0400

    8.91

--- Section8/pics/src/components/App.js
+++ Section8/pics/src/components/App.js
@@ -4,13 +4,15 @@ import SearchBar from './SearchBar';
 
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    async onSearchSubmit(term) {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID 3309e3bf897959a95859cd536ca50cb581b98d4e99be045572a862a27a2eb3cb'
             }
         });
+
+        console.log(response.data.results)
     }
 
     render() {
