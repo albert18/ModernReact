4e4f9d4be878e7971ec8f0ba37ab92938c549282
albert18 <caps18@hotmail.com>
2019-05-14 08:40:50 -0400

    section end

--- Section9/pics/src/components/ImageList.js
+++ Section9/pics/src/components/ImageList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const ImageList = (props) => {
+
     
     const images = props.images.map((image) => {
-        return <img alt="Cars" key={image.id} src={image.urls.regular} />
+        return <img alt={image.description} key={image.id} src={image.urls.regular} />
     });
 
 
