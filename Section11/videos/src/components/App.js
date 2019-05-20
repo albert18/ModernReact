eadfcb397d1fb6506eb8988136b06273752c8f71
albert18 <caps18@hotmail.com>
2019-05-19 20:47:34 -0400

    11.13

--- Section11/videos/src/components/App.js
+++ Section11/videos/src/components/App.js
@@ -5,9 +5,13 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
     render () {
         return (
-            <div className="ui container"><SearchBar /></div>
+            <div className="ui container">
+                <SearchBar />
+            </div>
         )
     }    
 }
 
 export default App;
+
+
