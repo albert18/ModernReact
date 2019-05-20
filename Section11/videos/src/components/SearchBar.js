81e1431388bf12e52139bff8f29890291b6f19cb
albert18 <caps18@hotmail.com>
2019-05-20 19:12:41 -0400

    11.115

--- Section11/videos/src/components/SearchBar.js
+++ Section11/videos/src/components/SearchBar.js
@@ -9,7 +9,8 @@ class SearchBar extends React.Component {
     onFormSubmit = event => {
         event.preventDefault();
 
-        
+        this.props.onFormSubmit(this.state.term);
+
     };
 
     render () {
