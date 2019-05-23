e04ce7cf8804398c1700942d0162457cb1996e1e
albert18 <caps18@hotmail.com>
2019-05-23 18:50:15 -0400

    11.27

--- Section11/videos/src/components/VideoDetail.js
+++ Section11/videos/src/components/VideoDetail.js
@@ -7,12 +7,10 @@ const VideoDetail = ({video}) => {
 
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
-
     return (
         <div>
             <div className="ui embed">
-                <iframe title="video" src={videoSrc} />
-
+                <iframe title="video player" src={videoSrc} />
             </div>
             <div className="ui segment">
             <h4 className="ui header">{video.snippet.title}</h4>
--- Section11/videos/src/components/VideoList.js
+++ Section11/videos/src/components/VideoList.js
@@ -2,8 +2,13 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideoList = ({videos, onVideoSelect}) => {
-    const renderedList = videos.map((video) => {
-        return <VideoItem onVideoSelect={onVideoSelect} video={video}/>
+    const renderedList = videos.map(video => {
+        return (
+            <VideoItem 
+                key={video.id.videoId} 
+                onVideoSelect={onVideoSelect} 
+                video={video}/>
+        ) 
     });
 
     return (
