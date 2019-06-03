82b2b44958a8a713684cc3235bd7b17ebe96d411
albert18 <caps18@hotmail.com>
2019-06-03 06:53:09 -0400

    15.180

--- section14/blog/src/components/PostList.js
+++ section14/blog/src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/index';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
     componentDidMount() {
@@ -12,9 +13,12 @@ class PostList extends React.Component {
             return (
                 <div className="item" key={post.id}>
                     <i className="large middle aligned icon user"/>
-                    <div className="description">
-                        <h2>{post.title}</h2>
-                        <p>{post.body}</p>
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                        <UserHeader userId={post.userId}/>
                     </div>
                 </div>
             );
