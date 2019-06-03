21ff975d852afbd0649b105dd42d0a6c296d49c0
albert18 <caps18@hotmail.com>
2019-06-03 06:42:25 -0400

    15.179

--- section14/blog/src/actions/index.js
+++ section14/blog/src/actions/index.js
@@ -1,10 +1,18 @@
 import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+import { async } from 'q';
 
 export const fetchPosts = () => async dispatch => {        
     const response = await jsonPlaceHolder.get('./posts');
     
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
     };
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload:response.data });
+}
+
     // return {
     //     type: 'FETCH_POSTS',
     //     payload: response
