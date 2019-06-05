d8ea8f7a7571dad2ede8e14cc35b593a37a5a9d6
albert18 <caps18@hotmail.com>
2019-06-05 09:52:38 -0400

    15.186

--- section14/blog/src/actions/index.js
+++ section14/blog/src/actions/index.js
@@ -8,25 +8,16 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-// export const fetchUser = (id) => async dispatch => {
-//     const response = await jsonPlaceHolder.get(`/users/${id}`);
+// ----- LODASH it Prevent from calling multiple calls APIn`
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
 
-//     dispatch({ type: 'FETCH_USER', payload:response.data });
-// }
+const _fetchUser = _.memoize(async(id, dispatch) => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload:response.data });
+});
 
 
-export const fetchUser = function(id) {
-    return async function(dispatch) {
-        const response = await jsonPlaceHolder.get(`/users/${id}`);
-
-        dispatch({ type: 'FETCH_USER', payload: response.data });
-    }
-}
-
-    // return {
-    //     type: 'FETCH_POSTS',
-    //     payload: response
-    // };
 
 
 
