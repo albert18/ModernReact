bce67fa5175fcd70e7d1e0751cb809c7df87199c
albert18 <caps18@hotmail.com>
2019-06-18 09:19:01 -0400

    19.230

--- section16/streams/client/src/components/streams/StreamCreate.js
+++ section16/streams/client/src/components/streams/StreamCreate.js
@@ -3,7 +3,7 @@ import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
     renderInput(formProps) {
-        console.log(formProps);
+        // renderInput({ input }) {
         return  <input {...formProps.input} />
         
         // <input 
