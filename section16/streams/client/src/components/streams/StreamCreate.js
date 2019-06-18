dd1585c765d6ad03251f69ec05adf1436ba956c7
albert18 <caps18@hotmail.com>
2019-06-18 09:25:52 -0400

    19.232

--- section16/streams/client/src/components/streams/StreamCreate.js
+++ section16/streams/client/src/components/streams/StreamCreate.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
-    renderInput(formProps) {
-        // renderInput({ input }) {
-        return  <input {...formProps.input} />
-        
-        // <input 
-        //         onChange={formProps.input.onChange} 
-        //         value={formProps.input.value} />
+        renderInput({ input, label }) {
+        return (
+            <div className="field">
+            <label>{label}</label>
+                <input {...input} />
+            </div>
+        )
     }
 
     render() {
         return (
-            <form>
-                <Field name="title" component={this.renderInput} />
-                <Field name="description" component={this.renderInput}/>
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
             </form>
         )
     }
