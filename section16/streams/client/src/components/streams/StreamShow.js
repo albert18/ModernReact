9ce178286eb1342bec81681ebed062a06961ecbc
albert18 <caps18@hotmail.com>
2019-07-25 21:00:19 -0400

    22.293

--- section16/streams/client/src/components/streams/StreamShow.js
+++ section16/streams/client/src/components/streams/StreamShow.js
@@ -11,7 +11,32 @@ class StreamShow extends React.Component {
     }
 
     componentDidMount() {
+        const { id } = this.props.match.params;
+        // console.log(this.videoRef)
+
         this.props.fetchStream(this.props.match.params.id);
+        this.buildPlayer();
+
+    }
+
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    buildPlayer() {
+        if (this.player || !this.props.stream) {
+            return;
+        }
+
+        const { id } = this.props.match.params;
+
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.play();
+
     }
 
     render () {
