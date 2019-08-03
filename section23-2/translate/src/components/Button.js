00e09d28058f2aff33360f2cd99047a3f188bc81
albert18 <caps18@hotmail.com>
2019-08-03 12:39:46 -0400

    23.302

--- section23-2/translate/src/components/Button.js
+++ section23-2/translate/src/components/Button.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import LanguageContext from './contexts/LanguageContext';
 
 class Button extends React.Component {
+    // note: must called 'contextType' exactly if not it won't work.
+    static contextType = LanguageContext;
+
     render() {
+        console.log(this.context);
         return (
             <button className="ui button primary">Submit</button>
         )
