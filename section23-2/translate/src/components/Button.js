fb8bea7bf31f56d3b4f69f57d6a0b6acdfa166d8
albert18 <caps18@hotmail.com>
2019-08-06 13:12:16 -0400

    24.313

--- section23-2/translate/src/components/Button.js
+++ section23-2/translate/src/components/Button.js
@@ -6,15 +6,15 @@ import ColorContext from './contexts/ColorContext';
 class Button extends React.Component {
     // note: must called 'contextType' exactly if not it won't work.
     // static contextType = LanguageContext;
-    renderSubmit(value) {
-        return value === 'english' ? 'Submit' : 'Voorleggen';
+    renderSubmit(language) {
+        return language === 'english' ? 'Submit' : 'Voorleggen';
     }
 
     rednerButton(color) {
         return (
             <button className={`ui button ${color}`}>
                 <LanguageContext.Consumer>
-                    {(value) => this.renderSubmit(value)}
+                    {({ language }) => this.renderSubmit(language)}
                 </LanguageContext.Consumer>
             </button>
         )
