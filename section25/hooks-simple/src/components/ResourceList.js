3f07a23044cc2841938a3140c2d5cbd0b78b9030
albert18 <caps18@hotmail.com>
2019-08-09 01:33:04 -0400

    25.330

--- section25/hooks-simple/src/components/ResourceList.js
+++ section25/hooks-simple/src/components/ResourceList.js
@@ -1,21 +1,7 @@
-import React, { useState, useEffect} from 'react';
-import axios from 'axios'
+import React from 'react';
+import useResources from './userResources';
 import { async } from 'q';
 
-const useResources = (resource) => {
-    const [resources, setResources] = useState([]);
-
-    useEffect(() => {
-        (async () => {
-            const response = await axios.get(
-                `https://jsonplaceholder.typicode.com/${resource}`
-                );
-                setResources(response.data);
-        })(resource)
-    }, [resource]);
-
-    return resources;
-};
 
 const ResourceList = ({ resource }) => {
     const resources = useResources(resource)
