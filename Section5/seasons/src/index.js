c1a9d3c6ee8d6d414008dceffe33cd747af38fd6
albert18 <caps18@hotmail.com>
2019-08-09 02:03:43 -0400

    update

--- Section5/seasons/src/index.js
+++ Section5/seasons/src/index.js
@@ -1,40 +1,66 @@
 /* eslint-disable no-useless-constructor */
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-class App extends React.Component {
+const App = () => {
+    const [lat, setLat] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    state = { lat: null, errorMessage: ''};
-
-    componentDidMount() {
+    useEffect(() => {
         window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude }),
-            err => this.setState({ errorMessage: err.message })
+            position => setLat(position.coords.latitude),
+            err => setErrorMessage(err.message)
         );
+    }, []);
+
+    let content;
+    if (errorMessage) {
+        content = <div>Error: {errorMessage}</div>;
+    } else if (lat) {
+        content = <SeasonDisplay lat={lat} />;
+    } else {
+        content = <Spinner message="Please accept location request"/>;
     }
 
-    renderContent() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: { this.state.errorMessage }</div>;
-        }
-
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />;
-        }
-
-        return <Spinner message="Please accept location request"/>;
-    }
-
-    //React says we have to define render!!!
-    render() {
-        return (
-            <div className="border red">
-                {this.renderContent()}
-            </div>
-        );
-    }
+    return <div className="border red">{content}</div>
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
+
+
+
+/// *************************BEFORE REFACTORING*************************SECTION 27 of Tutorial
+// class App extends React.Component {
+
+//     state = { lat: null, errorMessage: ''};
+
+//     componentDidMount() {
+//         window.navigator.geolocation.getCurrentPosition(
+//             position => this.setState({ lat: position.coords.latitude }),
+//             err => this.setState({ errorMessage: err.message })
+//         );
+//     }
+
+//     renderContent() {
+//         if (this.state.errorMessage && !this.state.lat) {
+//             return <div>Error: { this.state.errorMessage }</div>;
+//         }
+
+//         if (!this.state.errorMessage && this.state.lat) {
+//             return <SeasonDisplay lat={this.state.lat} />;
+//         }
+
+//         return <Spinner message="Please accept location request"/>;
+//     }
+
+//     //React says we have to define render!!!
+//     render() {
+//         return (
+//             <div className="border red">
+//                 {this.renderContent()}
+//             </div>
+//         );
+//     }
+// }
